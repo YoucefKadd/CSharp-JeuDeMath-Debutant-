21fc52fbf92de6d4d4039fd5f5163933d5f8bcea
Youcef k <youssefkaddour@hotmail.com>
2022-02-04 21:33:17 +0100

    Ajout de la fonction PoserQuestion()

--- JeuDeMath/JeuDeMath/Program.cs
+++ JeuDeMath/JeuDeMath/Program.cs
@@ -4,9 +4,28 @@ namespace JeuDeMath
 {
     class Program
     {
+        static void poserQuestion()
+        {
+            while (true) 
+            {
+                Console.Write("5 + 3 = ");
+                string reponse = Console.ReadLine();
+
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR : Vous devez rentrer un nombre");
+                }
+            }
+           
+        }
         static void Main(String[] args)
         {
-            Console.WriteLine("Hello World !");
+            poserQuestion();
         }
 
     }
