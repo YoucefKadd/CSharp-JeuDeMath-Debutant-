589c011826387cd79afaedbc17b49b1060b1f579
Youcef k <youssefkaddour@hotmail.com>
2022-02-04 21:19:32 +0100

    Ajout de la class MAIN et Hello World !

--- JeuDeMath/JeuDeMath/Program.cs
+++ JeuDeMath/JeuDeMath/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace JeuDeMath
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World !");
+        }
+
+    }
+   
+}
